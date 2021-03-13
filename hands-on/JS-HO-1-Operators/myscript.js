26027dc08ac553c913cdd170a4370db92ad180b7
sezginerdem <drsezginerdem@gmail.com>
2021-03-13 21:40:02 +0000

    add js variables

---
+++ hands-on/JS-HO-1-Operators/myscript.js
@@ -0,0 +1,19 @@
+//const myString = "hello" + 100 + "How are you"
+//const myString = `hello ${5+6} How are you`
+
+//console.log("myString: " + myString)
+
+/* const myVar_1 = 7
+const myVar_2 = "7"
+
+const myBool = myVar_1 === myVar_2
+
+console.log("myBool: " + myBool) */
+
+const myVar_1 = 6;
+const myVar_2 = 3.5;
+const myVar_3 = 5 + " Sezgin"
+
+console.log(`myVar_1: ${myVar_1} typeof: ${typeof myVar_1}`)
+console.log(`myVar_2: ${myVar_2} typeof: ${typeof myVar_2}`)
+console.log(`myVar_3: ${myVar_3} typeof: ${typeof myVar_3}`)
