9873c25ba2e348243fb4e2ba4408364b6075d82e
sezginerdem <drsezginerdem@gmail.com>
2021-03-14 07:49:21 +0000

    added operations

---
+++ hands-on/JS-HO-1-Operators/myscript3.js
@@ -0,0 +1,17 @@
+/* //OPERATIONS
+/* console.log(1+2) */
+
+/* const num_1 = 5
+const num_2 = 3
+
+const result = 5 + "6"
+
+console.log(result)
+console.log("MOD: ", num_1 % num_2) */
+
+/* console.log("=====================")
+
+let myNumber = 5;
+
+myNumber++;
+console.log(myNumber); */
