6b66b54d0157f58b1fb5af7d75d94b663f22a3d4
sezginerdem <drsezginerdem@gmail.com>
2021-03-14 07:09:38 +0000

    add string manipulating

---
+++ hands-on/JS-HO-1-Operators/myscript2.js
@@ -0,0 +1,67 @@
+//lenght
+const myString_1 = "Sezgin Erdem"
+const myString_2 = ""
+
+/* console.log("myString_1: " + myString_1.length)
+console.log("myString_2: " + myString_2.length) */
+
+//concat
+/* const myString_3 = "Sezgin" */
+/* const myString_4 = "Erdem"
+ */
+/* const complete = myString_3.concat(myString_4) */
+/* console.log(complete)
+console.log(complete.length) */
+
+
+const word = "Welcome to "
+/* console.log(word.concat(myString_3, "welcome")) */
+
+// CHAR-AT
+/* const myString_5 = "SezginErdem"
+console.log(myString_5[5] */
+
+//INCLUDES
+const myString_6 = "Hello friends, Welcome to UK";
+//console.log(myString_6.includes("friends"))
+
+//INDEX-OF
+const myString_7 = "This is life is my choice"
+/* console.log(myString_7.indexOf("life"));
+const text = "Selam";
+console.log(text.indexOf("S")); */
+
+//REPLACE
+const best = "Hello friends, Welcome to UK";
+//console.log(best)
+
+const newString = best.replace("Hello", "Hi");
+/* console.log(newString);
+console.log(best); */
+
+//SEARCH
+const myString_10 = "Hello friends, Welcome to this country"
+/* console.log(myString_10.indexOf("Welcome"));
+console.log(myString_10.search("Welcome"));
+ */
+
+//SLICE
+const myString_11 = "Welcome to the World!"
+const mySlice = myString_11.slice(1, 2);
+//console.log(mySlice)
+
+//SPLIT
+const myString_12 = "Hello friends"
+/* console.log(myString_12.split("e")); */
+
+//SUBSTR - SUBSTRING
+const myString_13 = "Hello frind Welcome to there"
+/* console.log(myString_13.substr(1, 3)) */
+
+//TOLOWERCASE
+const myString_14 = "HELLo WOrLD"
+/* console.log(myString_14.toLowerCase());
+ */
+//TRIM
+const myString_15 = "      hello World     ";
+console.log(myString_15.trim())
