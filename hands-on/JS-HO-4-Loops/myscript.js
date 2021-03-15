452cc9223b28d5c41d435fd48cb9b66c118998ce
sezginerdem <drsezginerdem@gmail.com>
2021-03-15 11:29:52 +0000

    loop

---
+++ hands-on/JS-HO-4-Loops/myscript.js
@@ -0,0 +1,17 @@
+/* for (let i = 0; i <= 5; i++) {
+    console.log("i: ", i);
+} */
+//WHILE
+/* let i = 0;
+
+while (i < 5) {
+    console.log("i: " + i)
+    i++
+} */
+
+//BREAK & CONTINUE
+/* 
+{
+    for (let i = 0; i < )
+
+} */
