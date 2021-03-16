b8c433c35cc7c5e0f16f92ce10470296fab15a14
sezginerdem <drsezginerdem@gmail.com>
2021-03-16 07:16:33 +0000

    add arrays

---
+++ hands-on/JS-HO-3-Arrays/myscript.js
@@ -0,0 +1,22 @@
+//ARRAY
+
+const myArray = ["London", "Swansea", "Cardiff", 2, false, 5, 7]; //flat array
+// console.log(myArray[2])
+
+//array of arra
+
+//object of array
+
+
+// {
+    // let arr_1 = ["Konya", "Canakkale"]
+    // const result = myArray.concat(arr_1)
+    // console.log(myArray);
+// }
+
+// {
+//     var desk = myArray.push("Van");
+//     console.log(desk);
+//     //console.log(myArray);
+// }
+
