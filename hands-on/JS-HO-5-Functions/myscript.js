ca66d9cbe90df6be6b4296bb02ba3c48c1463277
sezginerdem <drsezginerdem@gmail.com>
2021-03-16 14:10:49 +0000

    add function

--- hands-on/JS-HO-5-Functions/myscript.js
+++ hands-on/JS-HO-5-Functions/myscript.js
@@ -1,12 +1,32 @@
 //FUNCTIONS
 
-const myNumber_1 = 9;
-const myNumber_2 = 10;
+// const myNumber_1 = 9;
+// const myNumber_2 = 10;
 
-function sumNumbers(){
-    const result = myNumber_1 + myNumber_2;
-    return result;
-}
+// function sumNumbers(){
+//     const result = myNumber_1 + myNumber_2;
+//     return result;
+// }
 
-const res = sumNumbers();
-console.log(res);
+// const res = sumNumbers();
+// console.log(res);
+
+
+//REGULAR(NORMAL) FUNCTIOINS
+
+// function myFunction_1(p1) {
+//     console.log("myFunction_1! " + p1);
+// }
+
+//myFunction_1("Sezgin")
+
+//Function Expression
+// const myFunction_2 = function (p2) {
+//     console.log("myfunction_2 " + p2);
+// }
+
+// myFunction_2("Erdem");
+
+//arrow Function () => {}
+const myFunction_3 = p1 => p1 * p1
+console.log(myFunction_3(6));
