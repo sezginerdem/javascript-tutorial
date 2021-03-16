ca9bfef6e5a53ff37938212681d0999d6693c9e1
sezginerdem <drsezginerdem@gmail.com>
2021-03-16 13:20:06 +0000

    add functuons

---
+++ hands-on/JS-HO-5-Functions/myscript.js
@@ -0,0 +1,12 @@
+//FUNCTIONS
+
+const myNumber_1 = 9;
+const myNumber_2 = 10;
+
+function sumNumbers(){
+    const result = myNumber_1 + myNumber_2;
+    return result;
+}
+
+const res = sumNumbers();
+console.log(res);
